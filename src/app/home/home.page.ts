cdbd63146bf25879048f544a3d49282d276d9850
João Paulo Pena <jppenna04@gmail.com>
2021-03-04 20:02:44 -0300

    terminado a adição do resultado do imc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -21,7 +21,9 @@ export class HomePage {
     const imc = this.weight / (this.height * this.height);
     if(imc<18.5)this.showMessage(`IMC = ${imc.toFixed(2)}`+" - MAGREZA"); 
     else if (imc <24.9)this.showMessage(`IMC = ${imc.toFixed(2)}`+" - NORMAL");
-    
+    else if (imc <29.9)this.showMessage(`IMC = ${imc.toFixed(2)}`+" - SOBREPESO");
+    else if (imc <39.9)this.showMessage(`IMC = ${imc.toFixed(2)}`+" - OBESIDADE");
+    else this.showMessage(`IMC = ${imc.toFixed(2)}`+" - OBESIDADE GRAVE");
   }
 
   async showMessage(msg: string) {
